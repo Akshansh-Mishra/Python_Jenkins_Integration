2a7f048f549e4018f318e03c2b52dea03dc11cf8
Akshansh Mishra <89388513+Akshansh-Mishra@users.noreply.github.com>
2024-04-28 09:22:42 +0530

    Create basic-datatype.py

---
+++ basic-datatype.py
@@ -0,0 +1,25 @@
+# Define variables of different data types
+integer_var = 10
+float_var = 3.14
+string_var = "Hello, world!"
+list_var = [1, 2, 3, 4, 5]
+tuple_var = (6, 7, 8, 9, 10)
+dictionary_var = {'a': 1, 'b': 2, 'c': 3}
+boolean_var = True
+none_var = None
+set_var = {1, 2, 3, 4, 5}
+byte_var = b'binary data'
+complex_var = 2 + 3j
+
+# Print variables and their types
+print("Integer Variable:", integer_var, type(integer_var))
+print("Float Variable:", float_var, type(float_var))
+print("String Variable:", string_var, type(string_var))
+print("List Variable:", list_var, type(list_var))
+print("Tuple Variable:", tuple_var, type(tuple_var))
+print("Dictionary Variable:", dictionary_var, type(dictionary_var))
+print("Boolean Variable:", boolean_var, type(boolean_var))
+print("None Variable:", none_var, type(none_var))
+print("Set Variable:", set_var, type(set_var))
+print("Byte Variable:", byte_var, type(byte_var))
+print("Complex Variable:", complex_var, type(complex_var))
